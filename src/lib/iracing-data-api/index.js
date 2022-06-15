3b446c634b55909e222a1ed825ca569a6c437168
Kyle Crouse <kycrouse@amazon.com>
2022-06-15 00:12:50 +0000

    adding dir to path for dotenv

--- src/lib/iracing-data-api/index.js
+++ src/lib/iracing-data-api/index.js
@@ -1,7 +1,8 @@
 const axios = require('axios')
+const path = require('path')
 const { CookieJar } = require('tough-cookie')
 const { HttpCookieAgent, HttpsCookieAgent } = require('http-cookie-agent')
-const dotenv = require('dotenv').config()
+const dotenv = require('dotenv').config({ path: path.join(__dirname, '.env') })
 const CryptoJS = require('crypto-js')
 
 class Client {
