814f27a709626ea3ac90b2c53aee6e62e996a6f7
Kyle Crouse <kycrouse@amazon.com>
2022-06-15 00:04:24 +0000

    adding dir to file read

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const fs = require('fs')
+const path = require('path')
 const { Collection } = require('discord.js')
 const client = require('./src/lib/discord')
 
 client.commands = new Collection()
 
-const commandFiles = fs.readdirSync(`./src/commands`)
+const commandFiles = fs.readdirSync(path.join(__dirname, `./src/commands`))
 	.filter(file => file.endsWith('.js'))
 	
 for (const file of commandFiles) {
-	const command = require(`./src/commands/${file}`)
+	const command = require(path.join(__dirname, `./src/commands/${file}`))
 	client.commands.set(command.data.name, command)
 }
 
-const eventFiles = fs.readdirSync('./src/events')
+const eventFiles = fs.readdirSync(path.join(__dirname, './src/events'))
 	.filter(file => file.endsWith('.js'))
 
 for (const file of eventFiles) {
-	const event = require(`./src/events/${file}`)
+	const event = require(path.join(__dirname, `./src/events/${file}`))
 	if (event.once)
 		client.once(event.name, (...args) => event.execute(...args))
 	else
